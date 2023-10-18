92180c97701041c6b9f1c35bd829a233c6b37a82
Pooja Tikhe <tikhepooja11@gmail.com>
2023-10-19 00:52:48 +0530

    added description in comment

--- src/components/hooks_practice/UseRefHook/UserefVSnormalVSusestate.js
+++ src/components/hooks_practice/UseRefHook/UserefVSnormalVSusestate.js
@@ -55,3 +55,20 @@ const UserefVSnormalVSusestate = () => {
 };
 
 export default UserefVSnormalVSusestate;
+
+// useRef -> If we want to change the variable value like useState (using setFun) but dont want to re-render the component after updating the value. ie. unlike the useState.
+
+// scenario with normal behaviour ->
+// If we have two variables first is normal js variable declared using let & react state variable created using useState.
+// & If I updated the value of normal variable on button click it updates i  background but never updated the UI as does not re-renders the page unlike the state variable
+// after sometime if i updates the state variable using setfun then on every re-render component loads freshly so that normal variable gets updated with initial value it has assigned with.
+
+// Use of useRef ->
+// If we dont want our normal variable to be resetted on updating state variable using setfun then we need to use useRef.
+// but after increasing ref variable it increases in background but does not updates the UI on its own, we need to re-render the page by incrementing state variable so that react
+// remembers the last updated value of ref variable and immediately displays on UI.
+
+// syntax - const ref = useRef(initialValue)
+// console.log(ref) // {current : initialValue}
+
+// i.e when we creates variable with useRef then in actual it creates an object with current key setted to initial value.
