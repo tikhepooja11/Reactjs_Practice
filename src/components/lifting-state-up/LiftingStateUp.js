770021a108006ba2a1aabb99f69abc514d89000c
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-01-18 22:53:34 +0530

    Update LiftingStateUp.js

--- src/components/lifting-state-up/LiftingStateUp.js
+++ src/components/lifting-state-up/LiftingStateUp.js
@@ -1,3 +1,9 @@
+//  Lifting a state up -> It means lifting a state up to a common ancestor or higher level component, so that it can be shared with all the child components
+//  who needs to access it. 
+
+//  Purpose - It is used when multiple components need access to the same piece of state.
+//  By lifting the state to a common ancestor, you avoid duplicating state in multiple components.
+
 //  In this example, we have counter application in which state count is lifted up to a common ancestor "CounterApp".
 //  And both DisplayCounter and IncrementButton components accepts count state  & its updating function IncrementCount as props respectively.
 
