d9721a4c48e8678e44f94eced4afe94986aa6bff
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-01-19 08:31:16 +0530

    Update Debouncing.js

--- src/components/debouncing/Debouncing.js
+++ src/components/debouncing/Debouncing.js
@@ -8,3 +8,30 @@ Debouncing -> It is used to ensure that the time consuming tasks do not fire so 
   Instead of executing callback fun on every time event triggered, we will set a timer. 
   So that if the event occurs before that time, then that timer will be cancel and new timer will be setted
   And this way, callback function will be called only after timer expires.
+
+
+
+    function debounce(callback, delay) {
+  let timerId;
+
+  return function (...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
+
+// Example usage:
+const debounceFunction = debounce((searchTerm) => {
+  // Perform search or other time-consuming task
+  console.log(`Searching for: ${searchTerm}`);
+}, 300);
+
+// Assuming this is called on each keystroke in a search input
+debounceFunction("react");
+debounceFunction("reactjs");
+debounceFunction("react hooks");
+// The actual search function will be invoked only after 300 milliseconds without any new keystrokes.
+
