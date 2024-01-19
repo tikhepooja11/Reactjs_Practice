72ed6eea1601e84df28dfd749d9728b650a4810e
Pooja Tikhe <tikhepooja11@gmail.com>
2024-01-19 06:59:43 +0530

    changes accepted

--- src/components/Champions_league_team.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const data = axios
-  .get(
-    "https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League"
-  )
-  .then((res) => console.log(res.data))
-  .catch((error) => console.log(error));
