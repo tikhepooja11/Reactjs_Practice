4ad58828c89652b36322b637b8e8c5a3051c11ee
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-01-24 14:42:32 +0530

    Update HooksCounter.js

--- src/components/hooks_practice/UseState/HooksCounter.js
+++ src/components/hooks_practice/UseState/HooksCounter.js
@@ -1,5 +1,11 @@
 //    useState -> use to remember state of the component each nd every time though the same component is called multiple times
+//   incrementByFive() - when state updates depend on the previous state, it's generally safer to use the functional form of setCount to ensure correct behavior.
+//   functional form of setCount - function with prevCount argument / fun that receives the previous state
 
+//Asynchronous Nature:
+// Due to the asynchronous nature of state updates in React, the actual state updates may not happen immediately. React batches state updates for performance reasons.
+// Resulting State:
+// After the loop completes, React will process the state updates, and the count variable will be incremented by 5. The final state value will be count + 5.
 
 import React, { useState } from "react";
 import { BiPlusMedical } from "react-icons/bi";
@@ -12,6 +18,9 @@ const HooksCounter = () => {
       setCount((prev) => prev + 1);
     }
   };
+
+  // or const incrementByFive = () => (setCount((prevCount) => prevCount + 5)); but not or const incrementByFive = () => (setCount(count+ 5))
+
 
   //If we want to increment a value based on previous value always pass function with previous state as an argument
   return (
