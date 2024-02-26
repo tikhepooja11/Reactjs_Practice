fe59b8c6ad9084ad1e5aae86674c7612edc1d0d0
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-02-26 20:36:30 +0530

    Update Uncontrolled_Component.js

--- src/components/uncontrolled-component/Uncontrolled_Component.js
+++ src/components/uncontrolled-component/Uncontrolled_Component.js
@@ -1,3 +1,6 @@
+//  An uncontrolled component in React is a component where the state is not managed by React. 
+//  Instead, the state is directly controlled by the DOM. Uncontrolled components are often used when you want to integrate React with non-React code
+
 //  if we use useRef for inputs and let dom control the form elements unlike the form input to control the dom in controlled component
 //  It will change the value as per input form element but will not re-render the component unlike on state variable change
 import React, { useRef } from 'react';
